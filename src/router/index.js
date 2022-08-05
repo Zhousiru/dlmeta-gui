279d6907def14e4c59137dd5ca6dfe4675e8f9ea
Zhousiru <i@zhousiru.com>
2022-08-05 23:34:37 +0800

    cleanup template, add router

---
+++ src/router/index.js
@@ -0,0 +1,30 @@
+import { createRouter, createWebHashHistory } from "vue-router"
+
+import Home from "@/views/Home.vue"
+import Setting from "@/views/Setting.vue"
+import Detail from "@/views/Detail.vue"
+
+const routes = [
+    {
+        path: "/",
+        name: "Home",
+        component: Home,
+    },
+    {
+        path: "/setting",
+        name: "Setting",
+        component: Setting,
+    },
+    {
+        path: "/detail/:id",
+        name: "Detail",
+        component: Detail,
+    }
+];
+
+const router = createRouter({
+    routes,
+    history: createWebHashHistory()
+});
+
+export default router;
---
+++ src/views/Detail.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        <h1>Detail({{ $route.params.id }})</h1>
+    </div>
+</template>
+
+<style scoped>
+</style>
