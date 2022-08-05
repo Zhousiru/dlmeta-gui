a9cbabc8ce6da16cffa2615e2eca856cbdad50d1
Zhousiru <i@zhousiru.com>
2022-08-05 21:15:28 +0800

    initial commit

---
+++ main.cjs
@@ -0,0 +1,26 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: path.join(__dirname, 'preload.js')
+    })
+
+    win.loadFile('./dist/index.html')
+
+    win.webContents.openDevTools()
+}
+
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+      })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+  })
