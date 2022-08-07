3792d647d652c14b201d4caf4344d27b402e55f9
Zhousiru <i@zhousiru.com>
2022-08-08 00:14:40 +0800

    feat: get raw list

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,32 @@
-<script setup>
+<script>
+export default {
+    data() {
+        return {
+            rawList: []
+        }
+    },
+    async mounted() {
+        this.rawList = await window.electronAPI.getRawList()
+        console.log(this.rawList)
+        // TODO
+    },
+    methods: {
 
+    }
+}
 </script>
 
 <template>
-    <div>
-        <h1>Home</h1>
+    <div class="card">
+        <h1>概览</h1>
+    </div>
+    <div class="card" id="nav">
+        <button class="button button-outline" @click="this.$router.push('/setting')" style="margin-left: auto;">设置</button>
     </div>
 </template>
 
 <style scoped>
+#nav {
+    display: flex;
+}
 </style>
