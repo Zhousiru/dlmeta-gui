c260014da0c27323b02addef05b9709cdc41dfda
Zhousiru <i@zhousiru.com>
2022-08-09 01:44:03 +0800

    feat: get&list title of entris style: args of ipc functions

--- internal/ipc.cjs
+++ internal/ipc.cjs
@@ -1,7 +1,8 @@
 const fs = require('fs');
 const { dialog } = require('electron')
 
-const setting = require('./setting.cjs')
+const setting = require('./setting.cjs');
+const path = require('path');
 
 exports.readSetting = async () => {
     await fs.promises.writeFile("./setting.json", '', { flag: 'a' })
@@ -12,23 +13,34 @@ exports.readSetting = async () => {
     return JSON.parse(data.toString())
 }
 
-exports.saveSetting = async (...args) => {
-    let settingObj = args[1]
-
+exports.saveSetting = async (_, settingObj) => {
     await fs.promises.writeFile("./setting.json", JSON.stringify(settingObj, null, 4), { flag: 'w+' })
     setting.loadSetting()
 }
 
-exports.showOpenDialog = async (...args) => {
-    let props = args[1]
-
+exports.showOpenDialog = async (_, props) => {
     return dialog.showOpenDialog({ properties: props })
 }
 
-exports.getRawList = async () => {
-    let rawPath = setting.get('rawPath')
+exports.getFolderList = async (_, target) => {
+    let targetPath = setting.get(target)
 
-    let dir = fs.promises.readdir(rawPath)
+    let list = await fs.promises.readdir(targetPath, { withFileTypes: true })
+    let folder = []
+    list.forEach((file) => {
+        if (file.isDirectory()) folder.push(file.name)
+    })
 
-    return dir
+    return folder
+}
+
+exports.getDlmetaDetail = async (_, folder) => {
+    let data
+    try {
+        data = await fs.promises.readFile(path.join(setting.get('rawPath'), folder, '.dlmeta.json'))
+    } catch (e) {
+        if (e.code == 'ENOENT') return undefined
+    }
+
+    return JSON.parse(data.toString())
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,16 +2,43 @@
 export default {
     data() {
         return {
-            rawList: []
+            raw: {
+                detailed: [],
+                done: [],
+                original: []
+            }
         }
     },
-    async mounted() {
-        this.rawList = await window.electronAPI.getRawList()
-        console.log(this.rawList)
-        // TODO
+    mounted() {
+        this.getList()
     },
     methods: {
+        getList: async function () {
+            this.raw = {
+                detailed: [],
+                done: [],
+                original: []
+            }
 
+            let outputFolderList = await window.electronAPI.getFolderList('outputPath')
+            let rawFolderList = await window.electronAPI.getFolderList('rawPath')
+
+            rawFolderList.forEach(async folder => {
+                let detail = await window.electronAPI.getDlmetaDetail(folder)
+
+                if (!detail) {
+                    this.raw.original.push([folder, undefined])
+                    return
+                }
+
+                if (outputFolderList.includes((detail.title))) {
+                    this.raw.done.push([folder, detail])
+                    return
+                }
+
+                this.raw.detailed.push([folder, detail])
+            });
+        }
     }
 }
 </script>
@@ -21,12 +48,95 @@ export default {
         <h1>概览</h1>
     </div>
     <div class="card" id="nav">
-        <button class="button button-outline" @click="this.$router.push('/setting')" style="margin-left: auto;">设置</button>
+        <button class="button button-outline" @click="getList()">刷新</button>
+        <button class="button button-outline" @click="this.$router.push('/setting')"
+            style="margin-left: auto;">设置</button>
+    </div>
+    <div class="card list" id="list">
+        <div>未标记</div>
+        <ul>
+            <li v-for="i in raw.original">
+                <div class="entry-image"></div>
+                <div class="entry-overview">
+                    <div class="entry-title">{{ i[0] }}</div>
+                </div>
+            </li>
+        </ul>
+    </div>
+    <div class="card list">
+        <div>未处理</div>
+        <ul>
+            <li v-for="i in raw.detailed">
+                <div class="entry-image"></div>
+                <div class="entry-overview">
+                    <div class="entry-title">{{ i[1].title }}</div>
+                </div>
+            </li>
+        </ul>
+    </div>
+    <div class="card list">
+        <div>已完成</div>
+        <ul>
+            <li v-for="i in raw.done">
+                <div class="entry-image"></div>
+                <div class="entry-overview">
+                    <div class="entry-title">{{ i[1].title }}</div>
+                </div>
+            </li>
+        </ul>
     </div>
 </template>
 
 <style scoped>
 #nav {
     display: flex;
+}
+
+.list>div {
+    color: rgba(0, 0, 0, .5);
+}
+
+.list>ul {
+    list-style: none;
+    padding: 0;
+    margin: 1rem 0 0 0;
+}
+
+.list>ul>li {
+    height: 150px;
+    border-radius: 16px;
+    transition: all .2s;
+    display: flex;
+    overflow: hidden;
+}
+
+.list>ul>li:not(:first-child) {
+    margin-top: .6rem;
+}
+
+.list>ul>li:hover {
+    background-color: rgba(0, 0, 0, .05);
+    cursor: pointer;
+}
+
+.entry-image {
+    background-color: royalblue;
+    border-radius: 16px;
+    height: 150px;
+    width: 150px;
+}
+
+.entry-overview {
+    flex: 1;
+    padding: .8em;
+}
+
+.entry-title {
+    display: -webkit-box;
+    -webkit-line-clamp: 5;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+    word-break: break-all;
+    font-size: 1.2rem;
 }
 </style>
