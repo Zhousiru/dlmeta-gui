c2d0b1e0bccadb1a1bfd733c431e96a0620da368
Zhousiru <i@zhousiru.com>
2022-09-01 22:19:32 +0800

    style: no semicolon

--- preload.js
+++ preload.js
@@ -5,7 +5,7 @@ ipcRenderer.invoke('_getFn').then(r => {
     r.forEach(fn => {
         console.log(`[INFO] expose handler: ${fn}`)
         fnObj[fn] = (...args) => ipcRenderer.invoke(fn, ...args)
-    });
+    })
 
     contextBridge.exposeInMainWorld('electronAPI', fnObj)
 })
