6df9370ce57b149d354f12188c342a63759e07d3
Zhousiru <i@zhousiru.com>
2022-09-06 17:26:09 +0800

    feat: invalid setting hint

--- internal/setting.cjs
+++ internal/setting.cjs
@@ -4,8 +4,16 @@ let s
 
 exports.loadSetting = () => {
     console.log(`[INFO] load setting`)
-    fs.writeFileSync("./setting.json", '', { flag: 'a' })
-    let str = fs.readFileSync("./setting.json").toString()
+
+    const settingPath = './setting.json'
+
+    if (!fs.existsSync(settingPath)) {
+        fs.writeFileSync(settingPath, '{}', { flag: 'w' })
+    }
+
+    let str = fs.readFileSync(settingPath).toString()
+    if (!str.trim()) str = '{}'
+
     s = JSON.parse(str)
 }
 
